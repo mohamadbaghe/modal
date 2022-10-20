89b54d2b74f7cbb62c6210b0c7d7fc9efc50dd01
aliimthri <99986417+alimotahhari@users.noreply.github.com>
2022-10-20 12:03:25 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const openBtn = document.querySelector('.open');
+const modalContainer = document.querySelector('.modal-container')
+const closeBtn = document.querySelector('.modal-btn');
+
+
+openBtn.addEventListener("click",function(){
+    modalContainer.classList.add("show")
+})
+
+closeBtn.addEventListener('click',function(){
+    modalContainer.classList.remove("show");
+})
